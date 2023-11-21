fe1647ab7396476fd0ed3efd5187e9a42de96e02
ALAN BAYU <115530771+abes192@users.noreply.github.com>
2023-11-22 03:20:02 +0700

    Update process_message.php

--- process_message.php
+++ process_message.php
@@ -1,24 +1,41 @@
 <?php
-// Replace the connection details with your PostgreSQL information
-$conn = pg_connect("host=klompok3cloud-server.postgres.database.azure.com port=5432 dbname=klompok3cloud-database user=zbnbkxzkuj password=7D5N6PQ1VJWZW8L1$");
+try {
+    // PHP Data Objects(PDO) Connection
+    $pdo = new PDO("sqlsrv:server = tcp:cloud3webdatabase.database.windows.net,1433; Database = team_messages", "abes", "Alan12345@");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
+    // SQL Server Extension Connection
+    $connectionInfo = array(
+        "UID" => "abes",
+        "pwd" => "Alan12345@",
+        "Database" => "team_messages",
+        "LoginTimeout" => 30,
+        "Encrypt" => 1,
+        "TrustServerCertificate" => 0
+    );
+    $serverName = "tcp:cloud3webdatabase.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    // Process the submitted message and save it to the database
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $recipient = $_POST['recipient'];
+        $message = $_POST['message'];
+
+        $sql = "INSERT INTO messages (recipient, message) VALUES (?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$recipient, $message]);
+
+        echo "<p>Message sent successfully!</p>";
+    }
+} catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+} catch (Exception $ex) {
+    echo "Error: " . $ex->getMessage();
+} finally {
+    // Close the SQL Server connection
+    if (isset($conn)) {
+        sqlsrv_close($conn);
+    }
 }
-
-// Escape user inputs for security
-$recipient = pg_escape_string($_POST['recipient']);
-$message = pg_escape_string($_POST['message']);
-
-// Insert data into the database
-$sql = "INSERT INTO messages (recipient, message) VALUES ('$recipient', '$message')";
-
-if (pg_query($conn, $sql)) {
-    echo "<p>Message sent successfully!</p>";
-} else {
-    echo "Error: " . $sql . "<br>" . pg_last_error($conn);
-}
-
-// Close the database connection
-pg_close($conn);
 ?>
