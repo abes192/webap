5d23995ef44b3dcb2ab042bd7a48d6eb5fe21e56
ALAN BAYU <115530771+abes192@users.noreply.github.com>
2023-11-22 02:54:07 +0700

    Update process_message.php

--- process_message.php
+++ process_message.php
@@ -1,26 +1,24 @@
 <?php
-// Establish database connection (replace placeholders with actual values)
-// Ganti dengan informasi koneksi database yang sesuai
-$conn = new mysqli("localhost", "root", "", "team_messages");
+// Replace the connection details with your PostgreSQL information
+$conn = pg_connect("host=klompok3cloud-server.postgres.database.azure.com port=5432 dbname=klompok3cloud-database user=zbnbkxzkuj password=7D5N6PQ1VJWZW8L1$");
 
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
 }
 
 // Escape user inputs for security
-$recipient = $conn->real_escape_string($_POST['recipient']);
-$message = $conn->real_escape_string($_POST['message']);
+$recipient = pg_escape_string($_POST['recipient']);
+$message = pg_escape_string($_POST['message']);
 
 // Insert data into the database
 $sql = "INSERT INTO messages (recipient, message) VALUES ('$recipient', '$message')";
 
-if ($conn->query($sql) === TRUE) {
+if (pg_query($conn, $sql)) {
     echo "<p>Message sent successfully!</p>";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . pg_last_error($conn);
 }
 
 // Close the database connection
-$conn->close();
+pg_close($conn);
 ?>
